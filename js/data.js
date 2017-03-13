0e0d1983d5ed15c4720649da64da04a3c98b819f
WianB <wianbotha@gmail.com>
2017-03-13 14:51:18 +0200

    Created a seperate data json file
    Created a seperate data json file for the marker, map and other data.

---
+++ js/data.js
@@ -0,0 +1,216 @@
+//Map style
+
+var style = [
+    {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    }
+];
+
+
+// These are the real estate listings that will be shown to the user.
+// Normally we'd have these in a database instead.
+var locations = [{
+        title: 'Park Ave Penthouse',
+        location: {
+            lat: 40.7713024,
+            lng: -73.9632393
+        }
+    },
+    {
+        title: 'Chelsea Loft',
+        location: {
+            lat: 40.7444883,
+            lng: -73.9949465
+        }
+    },
+    {
+        title: 'Union Square Open Floor Plan',
+        location: {
+            lat: 40.7347062,
+            lng: -73.9895759
+        }
+    },
+    {
+        title: 'East Village Hip Studio',
+        location: {
+            lat: 40.7281777,
+            lng: -73.984377
+        }
+    },
+    {
+        title: 'TriBeCa Artsy Bachelor Pad',
+        location: {
+            lat: 40.7195264,
+            lng: -74.0089934
+        }
+    },
+    {
+        title: 'Chinatown Homey Space',
+        location: {
+            lat: 40.7180628,
+            lng: -73.9961237
+        }
+    }
+];
